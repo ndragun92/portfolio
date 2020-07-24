c08f38938b09d6e4b310bc62702850f81d3dfbb8
ndragun <39500267+ndragun92@users.noreply.github.com>
2020-07-24 21:22:30 +0200

    feat: GH script for deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/portfolio/'
+  }
+} : {}
 
 export default {
+  ...routerBase,
   /*
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
